672cba19f1b5048a35a498dc91e79cc2612a37b2
ridhni.mazumder.2004@gmail.com <ridhni.mazumder.2004@gmail.com>
2024-05-04 08:15:00 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -10,9 +10,7 @@ from anvil import Button, Image
 
 class Form1(Form1Template):
   def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    # self.youtube_video_1.youtube_id = 'FALlhXl6CmA'
     # Any code you write here will run before the form opens.
     self.count_click = 0
   
@@ -31,17 +29,13 @@ class Form1(Form1Template):
       self.map_1.add_component(self.markerList[ind])
       ind += 1
   
-      # wall stuff
-      #self.load_wall()
-      #self.drop_down_1.items = [('Hello',1),('There',2)]
 
   def marker_click(self, sender, **properties):
     self.infoAbtPin.visible=True
     self.count_click += 1
     self.infoAbtPin.text= 'You have clicked a pin ' + str(self.count_click) + ' time'
   
-  # Hide the wall elements and reset the wall label to the name of the location
-    self.label_b.text = sender.tag['Name']  # Update this line to set the wall label
+    self.label_b.text = sender.tag['Name']  
     self.label_b.visible = False
     self.repeating_panel_1.visible = False
     self.label_b.visible = True
@@ -57,7 +51,8 @@ class Form1(Form1Template):
     self.image_1.visible=True
     self.pictures = tables.app_tables.images.search(Location=sender.tag)
     self.location = sender.tag
-    self.image_1.source = self.pictures[self.count_click % len(self.pictures)]['Image']
+    self.image_1.source = self.pictures[self.count_click % len(self.pictures)]['ID']
+  
 
   def signBtn_click(self, **event_args):
     if len(self.text_box_1.text.strip())>0:
@@ -103,7 +98,6 @@ class Form1(Form1Template):
   def button_2_click(self, **event_args):
         """This method is called when the button is clicked"""
         alert("Baby Bevo wasn't a regular Bevo. He was a little squishmallow Bevo.")
-# Assuming you have two different images to switch between
 
   def button_3_click(self, **event_args):
     """This method is called when the button is clicked"""
@@ -115,24 +109,16 @@ class Form1(Form1Template):
      self.image_4.visible = True
      self.image_1.visible = False
   def form_show(self, **event_args):
-    # Set the minimum and maximum values of the slider
     self.slider_1.min = 0
     self.slider_1.max = 100
-    
-    # Set the initial value of the slider
-    self.slider_1.start = [10]  # Use a list for multiple handles if needed
-    
-    # Set the step value to determine the increments
+    self.slider_1.start = [10]
     self.slider_1.step = 10
     
-    # Optionally, set the format for displaying the value
     self.slider_1.format = "{:.0f}"
 
   def slider_1_change(self, **event_args):
-    # Get the current value of the slider
     slider_value = self.slider_1.value
     
-    # Reset visibility of images and labels
     self.image_4.visible = False
     self.image_a.visible = False
     self.image_6.visible = False
@@ -140,14 +126,13 @@ class Form1(Form1Template):
     self.label_a_im.text = ""
     self.label_6_im.text = ""
     
-    # Determine the visibility based on the slider's value
     if slider_value > 30:
         self.image_4.source = '_/theme/hen.gif'
         self.image_4.visible = True
         self.label_4_im.text = "Thanks"
       
         
-    if slider_value > 90:  # Replace with the value you choose
+    if slider_value > 90:  
         self.image_6.source = '_/theme/hen4.gif'
         self.image_6.visible = True
         self.label_6_im.text = "Coming"
@@ -161,3 +146,18 @@ image_source_1 = "_/theme/giphy.gif"
 image_source_2 = "_/theme/tenor.gif" 
 image_source_3 = "_/theme/hen.gif" 
 
+# Event handler for when an image is selected
+def on_image_selected(self, **event_args):
+    # Assuming 'selected_image_id' is the ID of the selected image
+    selected_image_id = self.images_table.selected_row['id'] # Retrieve this from your app logic
+
+    # Search for the image in the 'images' table by its ID
+    selected_image = app_tables.images.get(id=image_id)
+
+    # Check if the image was found
+    if selected_image:
+        # Update the label's text with the caption from the 'captions' column
+        self.label_r.text = selected_image['caption']
+    else:
+        # If the image is not found, display a default message or handle the error
+        self.label_r.text = "Caption not found."
