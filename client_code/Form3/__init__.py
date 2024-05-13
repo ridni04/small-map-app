90a43f1fe5338922656aa4bcb4e609b86add4190
ridhni.mazumder.2004@gmail.com <ridhni.mazumder.2004@gmail.com>
2024-05-13 00:13:31 +0000

    Created tenor (1).gif, sorry.gif and sorry-goma-cat-a1o3v6n5mwn924fn.gif; Edited Form1 and Form3

--- client_code/Form3/__init__.py
+++ client_code/Form3/__init__.py
@@ -12,3 +12,21 @@ class Form3(Form3Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.image_1.source = image_source_1
+    self.image_1.visible = True
+
+  def button_2_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.image_1.source = image_source_2
+    self.image_1.visible = True
+    
+  def button_3_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.image_1.source = image_source_3
+    self.image_1.visible = True
+
+image_source_1 = "_/theme/s.gif"
+image_source_2 = "_/theme/tenor (1).gif" 
+image_source_3 = "_/theme/sorry.gif" 
